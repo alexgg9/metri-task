070c8a318aa15abd23247fbe3900b12bc27910fa
alexgg9 <alexgalvezgarciaqt@gmail.com>
2025-06-05 17:56:11 +0200

    auth

--- app/Policies/ProjectPolicy.php
+++ app/Policies/ProjectPolicy.php
@@ -17,38 +17,26 @@ class ProjectPolicy
 
     public function create(User $user)
     {
-        return $user->role === 'manager' || $user->role === 'admin';
+        return $user->hasAnyRole(['manager', 'admin']);
     }
 
-    /**
-     * Determine if the user can view the project.
-     */
     public function view(User $user, Project $project)
     {
-        return $user->role === 'admin' || $project->users->contains($user);
+        return $user->hasRole('admin') || $project->users->contains($user);
     }
 
-    /**
-     * Determine if the user can update the project.
-     */
     public function update(User $user, Project $project)
     {
-        return $user->id === $project->user_id || $user->role === 'manager';
+        return $user->hasRole('manager') || $user->id === $project->user_id;
     }
 
-
-
-    /**
-     * Determine if the user can delete the project.
-     */
     public function delete(User $user, Project $project)
     {
-        return $user->role === 'admin' || $user->id === $project->user_id;
+        return $user->hasRole('admin') || $user->id === $project->user_id;
     }
-
 
     public function viewAny(User $user)
     {
-        return in_array($user->role, ['manager', 'admin']);
+        return $user->hasAnyRole(['manager', 'admin']);
     }
 }
--- config/cors.php
+++ config/cors.php
@@ -17,13 +17,11 @@ return [
 
     'paths' => ['*', 'api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'https://metri-task-react.vercel.app',
-    ],
+    'allowed_origins' => ['http://localhost:5173', "https://metri-task-react.vercel.app"],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => false,
+
 ];
